6b56edf42fcc0ca421c67c32dbe0c0e4d0e2ece1
Vishal-Kumar-Shaw <vkshaw1997@gmail.com>
2026-03-20 21:28:11 +0530

    Bug fixed

--- Day7_EasyProblems/easy-machine-coding-problems/src/components/Counter.tsx
+++ Day7_EasyProblems/easy-machine-coding-problems/src/components/Counter.tsx
@@ -5,51 +5,43 @@ const Counter = () => {
   const [min, setMin] = useState("-100000");
   const [max,setMax] = useState("100000");
   const [steps, setSteps] = useState("1");
-  const [disableInc, setDisableInc] = useState(false);
-  const [disableDec, setDisableDec] = useState(false);
+  // const [disableInc, setDisableInc] = useState(false);
+  // const [disableDec, setDisableDec] = useState(false);
+  const minVal = isNaN(Number(min)) ? -Infinity : Number(min);
+  const maxVal = isNaN(Number(max)) ? Infinity : Number(max);
+  const stepVal = isNaN(Number(steps)) ? 1 : Number(steps);
 
-
+  const isIncDisabled = count + stepVal > maxVal;
+  const isDecDisabled = count - stepVal < minVal;
 
   const handleIncrease = () =>{
-    let newVal = count + (parseInt(steps,10));
-    if( newVal > parseInt(max,10)){
-      setDisableInc(true);
-      return;
+    if(!isIncDisabled) {
+      setCount(prev => prev+stepVal);
     }
-    if(setDisableDec) setDisableDec(false);
-    setCount(newVal);
   } 
   
   const handleDecrease = () =>{
-    let newVal = count - (parseInt(steps,10));
-    if( newVal < parseInt(min,10) ){
-      setDisableDec(true);
-      setDisableInc(false);
-      return;
-    }
-    if(setDisableInc) setDisableInc(false);
-    setCount(newVal);
+   if(!isDecDisabled){
+      setCount(prev => prev-stepVal);
+   }
   }
   
   const handleReset = () =>{
     setCount(0);
-    setMax("100000");
-    setMin("-100000")
-    setSteps("1");
   }
 
   return (
     <div style={{ background: '#1B1F3A', width: '400px', borderRadius:'10px', padding:'20px'}}>
       <h1 style={{textAlign:'center', color:'white'}}>Counter App</h1>
       <div style={{display:'flex', maxWidth:'100%', height:'40px', color:'white', marginBottom:'20px', alignItems:'center'}}>
-        <p style={{height:'30px', fontSize:'18px'}}>Max: <input className="input-counter" type="text" value={max} onChange={(e)=>setMax(e.target.value)} /> </p>
-        <p style={{height:'30px', fontSize:'18px'}}>Min: <input className="input-counter" type="text" value={min} onChange={(e)=>setMin(e.target.value)}/> </p>
-        <p style={{height:'30px', fontSize:'18px'}}>Step: <input className="input-counter" type="text" value={steps} onChange={(e)=>setSteps(e.target.value)} /> </p>
+        <p style={{height:'30px', fontSize:'18px'}}>Max: <input className="input-counter" type="number" value={max} onChange={(e)=>setMax(e.target.value)} /> </p>
+        <p style={{height:'30px', fontSize:'18px'}}>Min: <input className="input-counter" type="number" value={min} onChange={(e)=>setMin(e.target.value)}/> </p>
+        <p style={{height:'30px', fontSize:'18px'}}>Step: <input className="input-counter" type="number" value={steps} onChange={(e)=>setSteps(e.target.value)} /> </p>
       </div>
       <div style={{display:'flex', width:'100%',  height:'80px', alignItems:'center'}}>
-        <button style={{width: '30%', fontSize:'40px', borderRadius:'8px'}} onClick={handleIncrease} disabled={disableInc}>+</button>
+        <button style={{width: '30%', fontSize:'40px', borderRadius:'8px'}} onClick={handleIncrease} disabled={isIncDisabled}>+</button>
          <p style={{width: '40%', textAlign:'center', fontSize:'30px', color:'white', border:'1px dashed white', height:'58%', borderRadius:'8px', alignContent:'center'}}>{count}</p>
-        <button style={{width: '30%', fontSize:'40px', borderRadius:'8px'}} onClick={handleDecrease} disabled={disableDec}>-</button>
+        <button style={{width: '30%', fontSize:'40px', borderRadius:'8px'}} onClick={handleDecrease} disabled={isDecDisabled}>-</button>
       </div>
       <button onClick={handleReset} style={{width: '100%', height: '60px', borderRadius:'10px', borderColor: 'none', outline: 'none', marginTop:'10px', cursor:'pointer', fontSize:'20px'}}>Reset</button>
     </div>
